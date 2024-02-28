f73b55cc741e28f180de0de4fdb7f7d36de70275
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-02-28 21:22:16 +0100

    initial commit

---
+++ src/test/java/com/artemnizhnyk/cqrsbankingapp/CqrsBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.cqrsbankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
