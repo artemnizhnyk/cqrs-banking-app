0f9f7a489f5f359c125e412ed263f006442086e1
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-04 12:35:56 +0200

    GsonConfig and LocalDateTimeDeserializer were added

---
+++ event-handler/src/main/java/com/artemnizhnyk/eventhandler/config/LocalDateTimeDeserializer.java
@@ -0,0 +1,30 @@
+package com.artemnizhnyk.eventhandler.config;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+@Component
+public class LocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
+
+
+    @SneakyThrows
+    @Override
+    public LocalDateTime deserialize(
+            final JsonElement jsonElement,
+            final Type type,
+            final JsonDeserializationContext jsonDeserializationContext
+    ) {
+        return LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(jsonElement.getAsLong() / 1000),
+                TimeZone.getDefault().toZoneId()
+        );
+    }
+}
