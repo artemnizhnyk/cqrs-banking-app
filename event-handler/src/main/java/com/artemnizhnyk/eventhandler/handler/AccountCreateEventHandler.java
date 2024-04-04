2fb20073349c835ac100f76d6e2d6374a5524321
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-04 12:47:25 +0200

    AccountCreateEventHandler was added

---
+++ event-handler/src/main/java/com/artemnizhnyk/eventhandler/handler/AccountCreateEventHandler.java
@@ -0,0 +1,27 @@
+package com.artemnizhnyk.eventhandler.handler;
+
+import com.artemnizhnyk.common.domain.model.Account;
+import com.artemnizhnyk.common.events.AccountCreateEvent;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component("ACCOUNT_CREATE")
+public class AccountCreateEventHandler implements EventHandler {
+
+    private final AccountService accountService;
+    private final Gson gson;
+
+    @Transactional
+    @Override
+    public void handle(final JsonObject object, final Acknowledgment acknowledgment) {
+        AccountCreateEvent event = gson.fromJson(object, AccountCreateEvent.class);
+        Account account = gson.fromJson((String) event.getPayload(), Account.class);
+        accountService.create(account);
+        acknowledgment.acknowledge();
+    }
+}
---
+++ event-handler/src/main/java/com/artemnizhnyk/eventhandler/handler/EventHandler.java
@@ -0,0 +1,9 @@
+package com.artemnizhnyk.eventhandler.handler;
+
+import com.google.gson.JsonObject;
+import org.springframework.kafka.support.Acknowledgment;
+
+public interface EventHandler {
+
+    void handle(final JsonObject object, final Acknowledgment acknowledgment);
+}
