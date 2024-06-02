ea2616f737de4837c18d8abc793dd3de4230e13b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-02 10:48:36 +0200

    some fetching and adding methods were added in CardRepository and ClientRepository

--- common/src/main/java/com/artemnizhnyk/common/repository/CardRepository.java
+++ common/src/main/java/com/artemnizhnyk/common/repository/CardRepository.java
@@ -2,6 +2,9 @@ package com.artemnizhnyk.common.repository;
 
 import com.artemnizhnyk.common.domain.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +15,14 @@ public interface CardRepository extends JpaRepository<Card, UUID> {
     boolean existsByNumberAndDate(final String number, final String date);
 
     Optional<Card> findByDateAndNumberAndCvv(final String number, final String date, final String cvv);
+
+    Optional<Card> findByNumberAndDate(final String number, final String date);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query(value = """
+            INSERT INTO cards_transactions
+            VALUES (:cardId, :transactionId)
+            """, nativeQuery = true)
+    void addTransaction(@Param("cardId") String cardId, @Param("transactionId") String transactionId);
+
 }
--- common/src/main/java/com/artemnizhnyk/common/repository/ClientRepository.java
+++ common/src/main/java/com/artemnizhnyk/common/repository/ClientRepository.java
@@ -2,6 +2,9 @@ package com.artemnizhnyk.common.repository;
 
 import com.artemnizhnyk.common.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +16,14 @@ public interface ClientRepository extends JpaRepository<Client, UUID> {
     boolean existsByUsername(final String username);
 
     Optional<Client> findByUsername(final String username);
+
+    Optional<Client> findByAccountId(UUID accountId);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query(value = """
+            INSERT INTO clients_cards
+            VALUES (:clientId, :cardId)
+            """, nativeQuery = true
+    )
+    void addCard(@Param("clientId") String clientId, @Param("cardId") String cardId);
 }
