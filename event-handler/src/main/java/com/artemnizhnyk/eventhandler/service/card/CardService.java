100b846dbf9495a2b241c287f6ce46d0221cad0f
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-02 11:43:45 +0200

    CardService was implemented in event-handler module

--- event-handler/src/main/java/com/artemnizhnyk/eventhandler/service/card/CardService.java
+++ event-handler/src/main/java/com/artemnizhnyk/eventhandler/service/card/CardService.java
@@ -2,7 +2,16 @@ package com.artemnizhnyk.eventhandler.service.card;
 
 import com.artemnizhnyk.common.domain.model.Card;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public interface CardService {
 
+    Card getById(final UUID id);
+
     Card create(final Card card);
+
+    void add(final Card card, final BigDecimal amount);
+
+    void addTransaction(final UUID cardId, final UUID transactionId);
 }
